f1670d378e89487c227cd607ca194440d84d285c
gurum505 <sayhojeong505@gmail.com>
2026-09-01 19:55:46 +0900

    cargo init

---
+++ hello_cargo/src/main.rs
@@ -0,0 +1,6 @@
+// cargo check를 주기적으로 한다음, 필요할 때 cargo build를 한다. 
+// cargo build --release를 사용하여 릴리즈 빌드를 생성할 수 있습니다.
+// 작동 속도를 벤치마킹할 시에는 릴리즈 빌드를 기준으로 해야 한다는 것도 알아두시기 바랍니다.
+fn main() {
+    println!("Hello, world!");
+}
--- main.rs
+++ main.rs
@@ -1,3 +1,6 @@
+// rustfmt main.rs  rs파일단독으로 formmatting
+// !는 매크로
+// rustc main.rs 컴파일-> pdb(window에서 디버깅 정보), exe(실행파일)
 fn main() {
     println!("Hello, world!");
 }
